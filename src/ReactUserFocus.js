bc767b016d225731c73eb807f0156c8371c6fce9
Sean Kilgarriff <seanpkilgarriff@gmail.com>
2016-11-21 16:00:07 -0600

    add moz hidden

--- src/ReactUserFocus.js
+++ src/ReactUserFocus.js
@@ -13,15 +13,18 @@ class ReactUserFocus extends React.Component {
 		// Set the visibility change event based on the browser.
 		this.isHidden;
 		this.visibilityChange;
-		if (typeof document.hidden !== 'undefined') {
-  			this.isHidden = 'hidden';
-  			this.visibilityChange = 'visibilitychange';
+		if (typeof document.hidden !== 'undefined') { // Opera 12.10 and Firefox 18 and later support
+    		hidden = 'hidden';
+    		visibilityChange = 'visibilitychange';
+		} else if (typeof document.mozHidden !== 'undefined') {
+		    hidden = 'mozHidden';
+		    visibilityChange = 'mozvisibilitychange';
 		} else if (typeof document.msHidden !== 'undefined') {
-  			this.isHidden = 'msHidden';
-  			this.visibilityChange = 'msvisibilitychange';
+		    hidden = 'msHidden';
+		    visibilityChange = 'msvisibilitychange';
 		} else if (typeof document.webkitHidden !== 'undefined') {
-  			this.isHidden = 'webkitHidden';
-  			this.visibilityChange = 'webkitvisibilitychange';
+		    hidden = 'webkitHidden';
+		    visibilityChange = 'webkitvisibilitychange';
 		}
 
 		this.checkFocusVis = this.checkFocusVis.bind(this);
