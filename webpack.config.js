e99dfd224a4a3dc8a31b8885236161389d4eebdc
Sean Kilgarriff <seanpkilgarriff@gmail.com>
2016-11-21 17:36:24 -0600

    add webpack, as well as yarn lock file for package management

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+/* jshint node: true */
+var path = require('path');
+
+
+module.exports = {
+  context: path.join(__dirname),
+  entry: './lib/index.js',
+
+  output: {
+    path: path.join(__dirname),
+    filename: '<%= slug %>.js',
+    libraryTarget: 'umd',
+    library: '<%= libname %>'
+  },
+
+  externals: {
+   'react': 'var React',
+   'react/addons': 'var React'
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.scss$/,
+        // Query parameters are passed to node-sass
+        loader: 'style!css!sass?outputStyle=expanded&' +
+          'includePaths[]=' + (path.resolve(__dirname, './bower_components')) + '&' +
+          'includePaths[]=' + (path.resolve(__dirname, './node_modules'))
+      },
+      {
+        test: /(\.js)|(\.jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          optional: ['runtime'],
+          stage: 0
+        }
+      }
+    ]
+  }
+};
