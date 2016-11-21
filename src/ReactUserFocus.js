e0d5dd9dd53549989c2f7cb6b123e0b0c2462143
Sean Kilgarriff <seanpkilgarriff@gmail.com>
2016-11-21 16:20:39 -0600

    clean up comments and add render all children

--- src/ReactUserFocus.js
+++ src/ReactUserFocus.js
@@ -1,11 +1,5 @@
 import React from 'react';
 
-//import { isSupported, visibility } from './visibility';
-
-//Have visibility change (The visibility change html5 api take precedence)
-
-//When reporting events back to the upper containers.
-
 class ReactUserFocus extends React.Component {
 
 	constructor(props){
@@ -14,32 +8,24 @@ class ReactUserFocus extends React.Component {
 		this.isHidden;
 		this.visibilityChange;
 		if (typeof document.hidden !== 'undefined') { // Opera 12.10 and Firefox 18 and later support
-    		hidden = 'hidden';
-    		visibilityChange = 'visibilitychange';
+    		this.isHidden = 'hidden';
+    		this.visibilityChange = 'visibilitychange';
 		} else if (typeof document.mozHidden !== 'undefined') {
-		    hidden = 'mozHidden';
-		    visibilityChange = 'mozvisibilitychange';
+		    this.isHidden = 'mozHidden';
+		    this.visibilityChange = 'mozvisibilitychange';
 		} else if (typeof document.msHidden !== 'undefined') {
-		    hidden = 'msHidden';
-		    visibilityChange = 'msvisibilitychange';
+		    this.isHidden = 'msHidden';
+		    this.visibilityChange = 'msvisibilitychange';
 		} else if (typeof document.webkitHidden !== 'undefined') {
-		    hidden = 'webkitHidden';
-		    visibilityChange = 'webkitvisibilitychange';
+		    this.isHidden = 'webkitHidden';
+		    this.visibilityChange = 'webkitvisibilitychange';
 		}
 
-		this.checkFocusVis = this.checkFocusVis.bind(this);
+		this.checkFocus = this.checkFocus.bind(this);
 	}
 
 	checkFocus() {
-		// console.log('From focus/blur');
-		// console.log(document.hasFocus());
-	}
-
-	checkFocusVis() {
-		console.log('From Visibility API');
-		console.log(document[this.isHidden]);
-		console.log(document.hasFocus());
-		if (document[this.isHidden] || document.hasFocus()) {
+		if (document[this.isHidden]) {
 			this.props.onChange(true);
   		} else {
     		this.props.onChange(false);
@@ -47,23 +33,18 @@ class ReactUserFocus extends React.Component {
 	}
 
 	componentWillMount() {
-		document.addEventListener(this.visibilityChange, this.checkFocusVis);
-		window.addEventListener('focus', this.checkFocus);
-		window.addEventListener('blur', this.checkFocus);
+		document.addEventListener(this.visibilityChange, this.checkFocus);
     }
 
 	componentWillUnmount() {
-		document.removeEventListener(this.visibilityChange, this.checkFocusVis);
-		window.removeEventListener('focus', this.checkFocus);
-		window.removeEventListener('blur', this.checkFocus);
+		document.removeEventListener(this.visibilityChange, this.checkFocus);
     }
 
 	render() {
 	        if (!this.props.children) {
 	            return null;
 	        }
-	        const child = React.Children.only(this.props.children);
-	        return React.cloneElement(child);
+	        return this.props.children;
 	    }
 
 }
