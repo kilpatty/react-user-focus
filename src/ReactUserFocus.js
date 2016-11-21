e1bbbdf8d63e72020c9c31b266d14579bc40bfe7
Sean Kilgarriff <seanpkilgarriff@gmail.com>
2016-11-21 00:06:25 -0600

    split focus callbacks

--- src/ReactUserFocus.js
+++ src/ReactUserFocus.js
@@ -2,6 +2,10 @@ import React from 'react';
 
 //import { isSupported, visibility } from './visibility';
 
+//Have visibility change (The visibility change html5 api take precedence)
+
+//When reporting events back to the upper containers.
+
 class ReactUserFocus extends React.Component {
 
 	constructor(props){
@@ -19,18 +23,24 @@ class ReactUserFocus extends React.Component {
 	}
 
 	checkFocus() {
+		console.log('From focus/blur');
+		console.log(document.hasFocus());
+	}
+
+	checkFocusVis() {
+		console.log('From Visibility API');
 		console.log(document.hasFocus());
 	}
 
 	componentWillMount() {
 		console.log(this.visibilityChange);
-		document.addEventListener(this.visibilityChange, this.checkFocus);
+		document.addEventListener(this.visibilityChange, this.checkFocusVis);
 		window.addEventListener('focus', this.checkFocus);
 		window.addEventListener('blur', this.checkFocus);
     }
 
 	componentWillUnmount() {
-		document.removeEventListener(this.visibilityChange, this.checkFocus);
+		document.removeEventListener(this.visibilityChange, this.checkFocusVis);
 		window.removeEventListener('focus', this.checkFocus);
 		window.removeEventListener('blur', this.checkFocus);
     }
