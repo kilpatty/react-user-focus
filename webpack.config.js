2f53a0ab2e962bdb2545b895bb68e1b1baf98755
Sean Kilgarriff <seanpkilgarriff@gmail.com>
2016-11-22 18:38:07 -0600

    switch library back to umd

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'index.js',
-    libraryTarget: 'commonjs',
+    libraryTarget: 'umd',
     library: ''
   },
   externals: {
